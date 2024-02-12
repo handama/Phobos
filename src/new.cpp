099d45328159a61343bf935e675a5046e8938f3c
handama <799550430@qq.com>
2024-02-12 16:58:38 +0800

    Update new.cpp

--- src/new.cpp
+++ src/new.cpp
@@ -1,63 +1,57 @@
 #include <YRPP.h>
 #include <Helpers/Macro.h>
-DEFINE_HOOK(4F4583, GScreenClass_DrawOnTop_TheDarkSideOfTheMoon, 6) {
+DEFINE_HOOK(4F4583, GScreenClass_DrawOnTop_TheDarkSideOfTheMoon, 6)
+{
 
 
-    static int idxFrame = 0;
+	static int idxFrame = 0;
 
-    RectangleStruct boundRect;
+	RectangleStruct boundRect;
 
-    Point2D Position;
+	Point2D Position;
 
-    SHPReference* pSHP;
+	SHPReference* pSHP;
 
-    char* filename = ScenarioClass::Instance->FileName;
-    int length = 0;
-    for (int i = 0; i < sizeof(filename); i++)
-    {
-        if (filename[i] == '.')
-        {
-            length = i + 1 + 4;
-        }
-    }
-    char* name = new char[length];
-    for (int i = 0; i < sizeof(filename); i++)
-    {
-        if (filename[i] != '.')
-            name[i] = filename[i];
-        else
-        {
-            name[i] = '.';
-            name[i+1] = 'S';
-            name[i+2] = 'H';
-            name[i+3] = 'P';
-            break;
-        }
-    }
-    pSHP = GameCreate<SHPReference>(name);
-    delete[]name; name = NULL;
+	char* filename = ScenarioClass::Instance->FileName;
+	char name[260];
+	memset(name, '\0', sizeof(name));
 
-    if (!pSHP)
+	for (int i = 0; i < strlen(filename) + 1; i++)
+	{
+		if (filename[i] == '.')
+		{
+			name[i] = '.';
+			name[i + 1] = 'S';
+			name[i + 2] = 'H';
+			name[i + 3] = 'P';
+			name[i + 4] = '\0';
+			break;
+		}
+		else
+			name[i] = filename[i];
+	}
+	pSHP = GameCreate<SHPReference>(name);
+	if (!pSHP)
+		return 0;
+	if (pSHP->Height == 0)
+		return 0;
 
-        return 0;
+	//游戏界面关联的Surface此时是Composite
 
-    //游戏界面关联的Surface此时是Composite
+	Position = { 0,DSurface::Composite->GetHeight() - pSHP->Height };
 
-    Position = { 0,DSurface::Composite->GetHeight() - pSHP->Height};
-
-    DSurface::Composite->DrawSHP(FileSystem::ANIM_PAL, pSHP, idxFrame, &Position,
-        DSurface::Composite->GetRect(&boundRect), BlitterFlags::TransLucent25, 0, 0, 0, 1000, 0, nullptr, 0, 0, 0);
+	DSurface::Composite->DrawSHP(FileSystem::ANIM_PAL, pSHP, idxFrame, &Position,
+		DSurface::Composite->GetRect(&boundRect), BlitterFlags::bf_400, 0, 0, 0, 1000, 0, nullptr, 0, 0, 0);
 
 
 
-    ++idxFrame %= pSHP->Frames;
+	++idxFrame %= pSHP->Frames;
 
 
 
-    GameDelete(pSHP);
-    return 0;
+	GameDelete(pSHP);
+	return 0;
 }
-
 
 
 
